6cb1c3ba5c32f7ff7ae681a131e58e8d0b01dc61
Markus Gerzer <mg@markusgerzer.de>
2022-10-08 14:16:06 +0200

    zoomComponent

--- examples/main.kt
+++ examples/main.kt
@@ -7,7 +7,8 @@ import com.soywiz.korgw.*
 import com.soywiz.korim.color.*
 import com.soywiz.korinject.*
 import com.soywiz.korma.geom.*
-import util.*
+import stageZoom.*
+import zoom.*
 import kotlin.reflect.*
 
 suspend fun main() = Korge(Korge.Config(module = ConfigModule))
@@ -19,9 +20,11 @@ object ConfigModule : Module() {
     override val scaleMode = ScaleMode.SHOW_ALL
     override val clipBorders = false
     override val scaleAnchor = Anchor.BOTTOM
-    override val mainScene: KClass<out Scene> = SceneA::class
+    //override val mainScene: KClass<out Scene> = SceneA::class
+    override val mainScene: KClass<out Scene> = SceneB::class
     override suspend fun AsyncInjector.configure() {
         mapPrototype { SceneA() }
+        mapPrototype { SceneB() }
     }
 }
 
@@ -56,5 +59,35 @@ class SceneA : Scene() {
                 }
             }
         }
+    }
+}
+
+class SceneB : Scene() {
+    override suspend fun SContainer.sceneInit() {
+        val gridSize = 9
+        val boxWidth = width / gridSize
+        val boxHeight = height / gridSize
+        for (row in 0 until gridSize) {
+            for (col in 0 until gridSize) {
+                solidRect(boxWidth, 1.0, Colors.BLUE) {
+                    onOver { color = Colors.VIOLET }
+                    onOut { color = Colors.BLUE }
+                }.xy(col * boxWidth, row * boxHeight)
+                solidRect(1.0, boxHeight, Colors.BLUE) {
+                    onOver { color = Colors.VIOLET }
+                    onOut { color = Colors.BLUE }
+                }.xy(col * boxWidth, row * boxHeight)
+            }
+        }
+        solidRect(10.0, 10.0, Colors.BLACK).xy(.0, .0)
+        solidRect(10.0, 10.0, Colors.BLACK).xy(.0, scaledWidth - 10.0)
+        solidRect(10.0, 10.0, Colors.BLACK).xy(scaledWidth - 10.0, .0)
+        solidRect(10.0, 10.0, Colors.BLACK).xy(scaledWidth - 10.0, scaledWidth - 10.0)
+        circle(5.0) {
+            color = Colors.BLUE
+            centerOnStage()
+        }
+        val zoomComponent = addZoomComponent(ZoomComponent(this))
+        //removeZoomComponent(zoomComponent)
     }
 }
